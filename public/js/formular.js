1f62d6f230403e634aa8fa4ea795462858a59e3c
Felix Hartmann <dealy_23@yahoo.de>
2014-09-24 16:02:34 +0200

    first try for delete and update button to mongodb

--- public/js/formular.js
+++ public/js/formular.js
@@ -1,20 +1,48 @@
 $(function() {
 
-  $("#bla").click(
-    function() {
+  $.ajax({
+    type: "GET",
+    url: "times",
+    success : function(data, status) {
+      for (i=0; i<data.length; i++) {
+        $(".ausgabe").append("<p>"+ data[i].date + "</p>"  + " " + data[i].from + " " + data[i].to + " " + data[i].lunch +
+          "<button type='button' class='btn btn-primary update'>/</button>" +
+          "<button id="+ data[i]._id +"' type='button' class='btn btn-primary delete'>X</button>")
+      }
+    }
+  });
 
+
+  $(".ausgabe").on( "click", ".delete", function(){
+    $.ajax({
+      type: "DELETE",
+      url: "time",
+      data: {
+        _id:($(this).attr('id'))
+      }
+
+
+    })
+
+
+  });
+
+  $("#submit").click(
+    function() {
       $.ajax({
         type: "POST",
         url: "save_time",
         data: {
+          date: $("#date").val(),
           from: $("#from").val(),
           to: $("#to").val(),
           lunch: $("#lunch").val()
         },
-        dataType: 'json'
+        dataType: 'json',
+        success : function(data, status) {
+          $(".ausgabe").append("<p>"+ data.date + " " + data.from + " " + data.to + " " + data.lunch+"</p>");
+        }
       });
-
-      $(".ausgabe").append("<p>"+from + " " + to + " " + lunch+"</p>");
     }
   )
 });
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express')
   , app = express()
   , ejs = require('ejs')
-  , bodyParser = require('body-parser');
+  , bodyParser = require('body-parser')
+  , mongojs = require('mongojs');
 
+var databaseUrl = "mydb";
+var collections = ["times"];
+var db = mongojs.connect(databaseUrl, collections);
 
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
@@ -13,9 +17,30 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
+
+
 app.post('/save_time', function(req, res){
 
-  console.log(req.body);
+  db.times.insert(req.body, function(err, document) {
+    res.send(document);
+  });
+});
+
+
+app.get('/times', function(req, res){
+
+  db.times.find({}, function(err, data) {
+    res.send(data);
+  });
+
+});
+
+app.delete('/time', function(req, res){
+
+    db.times.remove(req.body, function(err, document1){
+      res.send(document1);
+
+  })
 });
 
 var server = app.listen(3000, function() {
