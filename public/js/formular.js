5fc0f2360472ffc2ce8939dc293e56fd9a30cedc
Felix Hartmann <dealy_23@yahoo.de>
2014-09-25 11:36:43 +0200

    delete of the HTML Line after DB delete works now

--- public/js/formular.js
+++ public/js/formular.js
@@ -5,26 +5,32 @@ $(function() {
     url: "times",
     success : function(data, status) {
       for (i=0; i<data.length; i++) {
-        $(".ausgabe").append("<p>"+ data[i].date + "</p>"  + " " + data[i].from + " " + data[i].to + " " + data[i].lunch +
-          "<button type='button' class='btn btn-primary update'>/</button>" +
-          "<button id="+ data[i]._id +" type='button' class='btn btn-primary delete'>X</button>")
+        $(".ausgabe").append("<div id=1"+ data[i]._id+">" + data[i].date +  " " + data[i].from + " " + data[i].to + " " + data[i].lunch +
+          "<button type='button' class='btn btn-primary update " + data[i]._id +"'>/</button>" +
+          "<button id=" + data[i]._id +" type='button' class='btn btn-primary delete'>X</button>" + "</div>")
       }
     }
   });
 
 
   $(".ausgabe").on( "click", ".delete", function(){
+    var iddelete = ($(this).attr('id'));
+    console.log(iddelete);
     $.ajax({
       type: "DELETE",
       url: "time",
       data: {
-        _id:($(this).attr('id'))
+        _id: iddelete
       },
-      success: function(document1, status, buttton){
-        //Remove line in HTML
+      success: function (document1, status, button) {
+        $("div #1" +iddelete).remove()
       }
     })
   });
+
+
+
+
 
   $("#submit").click(
     function() {
